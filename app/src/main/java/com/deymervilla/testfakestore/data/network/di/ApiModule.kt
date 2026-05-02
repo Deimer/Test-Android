2170766f730fc7ed47bbc2fa66917f8813711943
deimer215@gmail.com <deimer215@gmail.com>
2026-05-02 12:02:27 -0500

    Implemented ApiModule to centralize network dependency injection using Hilt. Configured providers for OkHttpClient with logging interceptors, the Retrofit instance consuming BuildConfig.BASE_URL, and the creation of the ApiService to be injected into the data layer.

---
+++ app/src/main/java/com/deymervilla/testfakestore/data/network/di/ApiModule.kt
@@ -0,0 +1,48 @@
+package com.deymervilla.testfakestore.data.network.di
+
+import com.deymervilla.testfakestore.BuildConfig
+import com.deymervilla.testfakestore.data.network.api.ApiService
+import com.deymervilla.testfakestore.data.network.constants.NetworkConstants.DEFAULTS.DEFAULT_TIMEOUT
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ApiModule {
+
+    @Provides
+    fun provideOkHttpClientApi(): OkHttpClient {
+        val logging = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+        return OkHttpClient.Builder()
+            .callTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS)
+            .readTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS)
+            .connectTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS)
+            .retryOnConnectionFailure(true)
+            .apply { if(BuildConfig.DEBUG) addInterceptor(logging) }
+            .build()
+    }
+
+    @Provides
+    fun provideRetrofitApi(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .client(provideOkHttpClientApi())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    fun provideApiApi(): ApiService {
+        return provideRetrofitApi()
+            .create(ApiService::class.java)
+    }
+}
